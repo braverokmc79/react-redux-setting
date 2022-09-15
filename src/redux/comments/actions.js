ec0d8a19dba9cadab14cdae363157129e9a12374
braverokmc79 <braverokmc79@gmail.com>
2022-09-15 16:56:50 +0900

    1.리액트 리덕스(Redux)실습강좌

--- src/redux/comments/actions.js
+++ src/redux/comments/actions.js
@@ -3,6 +3,7 @@ import { FETCH_COMMENTS, FETCH_COMMENTS_REQUEST, FETCH_COMMENTS_SUCCESS, FETCH_C
 //thunk 사용 설정으로 dispatch 함수 설정이 가능함
 export const fetchComments = () => {
     return (dispatch) => {
+        //동기식으로 순차적으론 진행 가능.
         dispatch(fetchCommentRequest());
 
         fetch("https://jsonplaceholder.typicode.com/comments")
